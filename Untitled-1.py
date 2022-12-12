bdc574c276e94ce0bca1af15feaa769d4d64a397
Dariushavi <86901004+Dariushavi@users.noreply.github.com>
2022-12-12 05:22:47 -0700

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,6 @@
+# flow rate = 100 units/second
+# time = 10 seconds
+# flow area = 1 square meter
+# fluid density = 1000 kilograms/cubic meter
+h = mud_pulse_height(100, 10, 1, 1000)
+print(h)  # should print 10
